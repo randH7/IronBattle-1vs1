999c4135f8e3578d6d0a66bcbfaa32001810fb1f
Manar <manar@MacBook-Pro-H.local>
2023-10-01 17:00:16 +0300

    create character class and attecker interface

--- src/Attacker.java
+++ src/Attacker.java
@@ -1,2 +1,3 @@
 public interface Attacker {
+    void attack(Character character);
 }
--- src/Character.java
+++ src/Character.java
@@ -1,2 +1,53 @@
+import  java.util.Random;
+import java.util.UUID;
+
 public class Character {
+    private final String id;
+    private String name;
+    private int hp ;
+    private boolean isAlive ;
+    public Character(String name, int hp){
+        this.id=generateId();
+        this.name=name;
+        this.hp=hp;
+        this.isAlive =true;
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public void setAlive(boolean alive) {
+        isAlive = alive;
+    }
+    private String generateId() {
+        UUID uuid = UUID.randomUUID();
+        return uuid.toString();
+    }
+
 }
+
+
